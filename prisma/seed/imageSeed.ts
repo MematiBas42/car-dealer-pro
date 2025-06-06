d2aed811f3a5fbbac9869b6d59ff9767b7282ebe
vvduth <ducthai060501@gmail.com>
2025-06-06 20:00:47 +0300

    feat: add image seeding functionality and placeholder image source

---
+++ prisma/seed/imageSeed.ts
@@ -0,0 +1,19 @@
+import { imageSources } from "@/config/constants";
+import { faker } from "@faker-js/faker";
+import { Prisma, PrismaClient } from "@prisma/client";
+
+export async function seedImage(prisma: PrismaClient) {
+    const cars = await prisma.classified.findMany({})
+
+    const carids = cars.map(car => car.id);
+    for (const carId of carids) {
+        const image : Prisma.ImageCreateInput = {
+            src: imageSources.classifiedPlaceholder,
+            alt: faker.lorem.words(3),
+            classified: {
+                connect: { id: carId }
+            },
+            blurhash:'DNcFG4g9lXWTewfX/Wndj8Y='
+        }
+    }
+}
--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import { seedTax } from "./tax.seed";
+import { seedClass } from "./class.seed";
 
 const prisma = new PrismaClient();
 async function main() {
-    await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE;`;
-    await seedTax(prisma)
+    // await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE;`;
+    // await seedTax(prisma)
+
+    await seedClass(prisma);
+
+
 }
 
 main().catch(async (e) => {
