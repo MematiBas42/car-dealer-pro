f5c00e8c02dc92e2017fe569d8f2a97b641e0c5d
vvduth <ducthai060501@gmail.com>
2025-06-11 11:25:52 +0300

    feat: add LatestArrival and OurBrandSection components, integrate LatestArrivalsCarousel for displaying latest cars, and enhance homepage layout

--- src/app/(presentation)/page.tsx
+++ src/app/(presentation)/page.tsx
@@ -2,6 +2,8 @@
 import { PageProps } from "@/config/types";
 import HeroSection from "@/components/homepage/hero-section";
 import { FeaturesSection } from "@/components/homepage/features-section";
+import LastestArrival from "@/components/homepage/lastest-arrival";
+import OurBrandSection from "@/components/homepage/our-brands-section";
 export default async function Home(props: PageProps) {
   const searchParams = await props.searchParams;
 
@@ -10,6 +12,8 @@ export default async function Home(props: PageProps) {
     <div className="w-full min-h-screen bg-background">
       <HeroSection searchParams={searchParams} />
       <FeaturesSection />
+      <LastestArrival />
+      <OurBrandSection  />
     </div>
   );
 }
---
+++ src/components/homepage/LatestArrivalCarousel.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import type { CarWithImages } from "@/config/types";
+import dynamic from "next/dynamic";
+import "swiper/css";
+import { Prisma } from "@prisma/client";
+import { Navigation } from "swiper/modules";
+import { SwiperSlide } from "swiper/react";
+import CarCard from "../inventory/car-card";
+import SwiperButton from "../shared/swiper-button";
+
+interface LatestArrivalCarouselProps {
+  cars: CarWithImages[];
+  favourites: number[];
+}
+const Swiper = dynamic(() => import("swiper/react").then((mod) => mod.Swiper), {
+  ssr: false,
+  loading: () => (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-8">
+      {[1, 2, 3, 4].map((i) => (
+        <div key={i}>
+            Loading...
+        </div>
+      ))}
+    </div>
+  ),
+});
+
+export const LatestArrivalsCarousel = (props: LatestArrivalCarouselProps) => {
+    const { cars, favourites } = props;
+
+  return (
+    <div className="mt-8 relative">
+        <Swiper
+				navigation={{
+					prevEl: ".swiper-button-prev",
+					nextEl: ".swiper-button-next",
+				}}
+				pagination={{ clickable: true }}
+				modules={[Navigation]}
+				loop={true}
+				spaceBetween={30}
+				slidesPerView={1}
+				breakpoints={{
+					640: {
+						slidesPerView: 2,
+					},
+					1024: {
+						slidesPerView: 3,
+					},
+					1536: {
+						slidesPerView: 4,
+					},
+				}}
+			>
+				{cars.map((car) => {
+					return (
+						<SwiperSlide key={car.id}>
+							<CarCard car={car} favourites={favourites} />
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+			<SwiperButton
+				prevClassName="-left-16 border border-2 border-border hidden lg:flex"
+				nextClassName="-right-16 border border-2 border-border hidden lg:flex"
+			/>
+    </div>
+  )
+};
